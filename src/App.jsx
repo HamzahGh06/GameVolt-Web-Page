1170d2d9267587338cab0a380de7e4a2c3e957f5
BasharSaleh99 <basharshabaneh0@gmail.com>
2026-05-05 23:12:39 +0300

    edit the file.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
-import Home from './pages/Home';
+import Home from './pages/Home'
 import About from './pages/About';
 import Login from './pages/Login';
 import Register from './pages/Register';
